2cc2cb1e2a1886cc8bab7283cdf7c505919a923e
Nick Saway <rnsaway@gmail.com>
2018-01-05 14:14:17 -0700

    making PiPy package commits - round2

--- setup.py
+++ setup.py
@@ -8,5 +8,8 @@ setup(
     license='The Unlicense',
     author='Nick Saway',
     author_email='rnsaway@gmail.com',
-    description='A python connection to MOCA'
+    description='A python connection to MOCA',
+    keywords='moca wms',
+    install_requires=['pandas', 'requests', 'xml'],
+    python_requires='>=3'
 )
